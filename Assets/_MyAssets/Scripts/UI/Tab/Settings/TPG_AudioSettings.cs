c21b50549501971802b031c411951131b7786a61
주휘중 <gnlwnd5084@gmail.com>
2024-02-13 23:59:46 +0900

    [#2] 오디오 설정 구현

--- Assets/_MyAssets/Scripts/UI/Tab/Settings/TPG_AudioSettings.cs
+++ Assets/_MyAssets/Scripts/UI/Tab/Settings/TPG_AudioSettings.cs
@@ -1,8 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TPG_AudioSettings : TabPage
 {
-    
+    private enum EVolumeSlider
+    {
+        Master,
+        Bgm,
+        Se,
+    }
+
+    [SerializeField] private List<Slider> _volumeSliders;
+
+    private void Awake()
+    {
+        for (var sliderType = EVolumeSlider.Master; sliderType <= EVolumeSlider.Se; sliderType++)
+        {
+            EVolumeSlider type = sliderType; // 람다 캡처용 복사
+            _volumeSliders[(int)type].value = PlayerPrefs.GetFloat(GetPlayerPrefsKeyNameBySliderEnum(type), 1.0f);
+            _volumeSliders[(int)type].onValueChanged.AddListener((value) => { AdjustVolume(type, value); });
+        }
+    }
+
+    private void AdjustVolume(EVolumeSlider sliderType, float value)
+    {
+        _volumeSliders[(int)sliderType].value = value;
+        PlayerPrefs.SetFloat(GetPlayerPrefsKeyNameBySliderEnum(sliderType), value);
+    }
+
+    private string GetPlayerPrefsKeyNameBySliderEnum(EVolumeSlider slider)
+    {
+        switch (slider)
+        {
+            case EVolumeSlider.Master:
+                return PlayerPrefsKeyNames.MASTER_VOLUME;
+            case EVolumeSlider.Bgm:
+                return PlayerPrefsKeyNames.BGM_VOLUME;
+            case EVolumeSlider.Se:
+                return PlayerPrefsKeyNames.SE_VOLUME;
+            default:
+                Debug.Assert(false);
+                return null;
+        }
+    }
 }
--- Assets/_MyAssets/Scripts/Utils/PlayerPrefsKeyNames.cs
+++ Assets/_MyAssets/Scripts/Utils/PlayerPrefsKeyNames.cs
@@ -1,8 +1,7 @@
 public static class PlayerPrefsKeyNames
 {
+    public const string MASTER_VOLUME = "MasterVolume";
     public const string BGM_VOLUME = "BgmVolume";
     public const string SE_VOLUME = "SeVolume";
-    public const string VIBRATION_DISABLED = "VibrationDisabled";
     public const string VIGNETTE_STRENGTH = "Vignette";
-    public const string HIGH_SCORE = "HighScore";
 }
