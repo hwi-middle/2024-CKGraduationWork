32201e8f859253e9d3b0c215c445f91077e7e5dc
Jae_U <31125543+thejae-u@users.noreply.github.com>
2024-02-22 03:00:13 +0900

    [#11] 경사로 버그 수정

--- Assets/_MyAssets/Scripts/Player/PlayerMove.cs
+++ Assets/_MyAssets/Scripts/Player/PlayerMove.cs
@@ -7,102 +7,102 @@ namespace _MyAssets.Scripts.Player
     public class PlayerMove : MonoBehaviour
     {
         [SerializeField] private float _moveSpeed;
-        [SerializeField] private float _jumpSpeed;
+        [SerializeField] private float _jumpPower;
         [SerializeField] private float _slideSpeed;
 
+
+        [Header("Gravity Scale")]
+        [SerializeField] private float _gravityMultiplier;
+        private float _yVelocity;
+        
         private Camera _camera;
         
         private Vector3 _inputDirection;
+        private Vector3 _velocity;
 
         private CharacterController _controller;
-        private Vector3 _moveDirection;
         private Vector3 _hitNormal;
 
-        
-        private bool IsGround
-        {
-            get
-            {
-                Debug.Assert(_controller != null, "_controller != null");
-                
-                return _controller.isGrounded;
-            }
-        }
+        private bool IsGround => _controller.isGrounded;
 
         private bool IsLimitSlope
         {
             get
             {
-                Debug.Assert(_controller != null, "_controller != null");
-
                 float angle = Vector3.Angle(Vector3.up, _hitNormal);
-                return angle <= _controller.slopeLimit && angle != 0;
+                return angle >= _controller.slopeLimit && angle <= 90 && IsGround;
             }
         }
 
-        private bool HasInput
+        private void Awake()
         {
-            get
-            {
-                return _inputDirection != Vector3.zero;
-            }
+            _controller = GetComponent<CharacterController>();
+            _camera = Camera.main;
         }
 
         private void Start()
         {
-            _controller = GetComponent<CharacterController>();
-            _moveDirection = Vector3.zero;
-
-            _camera = Camera.main;
-            
-            // Cursor Invisible
+            // Cursor Visible
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
 
-        private void FixedUpdate()
+        private void Update()
         {
+            if (IsLimitSlope)
+            {
+                SlidePlayer();
+                return;
+            }
+
+            if (_inputDirection.sqrMagnitude == 0 && IsGround)
+            {
+                return;
+            }
+            
             RotatePlayer();
             MovePlayer();
         }
 
-        private void MovePlayer()
-        {
-            // 가파른 경사로에서는 미끄러짐
-            if (IsLimitSlope && IsGround)
-            {
-                _moveDirection.x += (1f - _hitNormal.y) * _hitNormal.x;
-                _moveDirection.z += (1f - _hitNormal.y) * _hitNormal.z;
-                _controller.Move(_moveDirection * (_slideSpeed * Time.fixedDeltaTime));
-                return;
-            }
-            
-            // 입력이 아무것도 없을 때 공중에 있지 않으면 연산하지 않음
-            if (!HasInput && IsGround)
-            {
-                return;
-            }
-            
-            _inputDirection.y = _moveDirection.y;
-            _moveDirection = transform.TransformDirection(_inputDirection);
-            
-            _moveDirection.y += Physics.gravity.y * Time.fixedDeltaTime;
-            _controller.Move(_moveDirection * (_moveSpeed * Time.fixedDeltaTime));
-        }
-
         private void RotatePlayer()
         {
-            if (!HasInput)
-            {
-                return;
-            }
-            
             Debug.Assert(_camera != null, "_camera != null");
             
             Quaternion cameraRotation = _camera.transform.localRotation;
             cameraRotation.x = 0;
             cameraRotation.z = 0;
             transform.rotation = Quaternion.Slerp(transform.rotation, cameraRotation, 1.0f);
+        }
+
+        private void SlidePlayer()
+        {
+            float yInverse = 1f - _hitNormal.y;
+            _velocity.x += yInverse * _hitNormal.x;
+            _velocity.z += yInverse * _hitNormal.z;
+            _controller.Move(_velocity * (_slideSpeed * Time.deltaTime));
+        }
+
+        private void MovePlayer()
+        {
+            _velocity = transform.TransformDirection(_inputDirection);
+            
+            GravityCalculate();
+            
+            _controller.Move(_velocity * (_moveSpeed * Time.deltaTime));
+        }
+
+        private void GravityCalculate()
+        {
+            if (IsGround && _yVelocity < 0.0f)
+            {
+                _yVelocity = -1.0f;
+            }
+            else
+            {
+                _yVelocity += Physics.gravity.y * _gravityMultiplier * Time.deltaTime;
+            }
+            
+            _velocity.y = _yVelocity;
         }
 
         public void OnMove(InputAction.CallbackContext context)
@@ -114,17 +114,22 @@ namespace _MyAssets.Scripts.Player
 
         public void OnJump(InputAction.CallbackContext context)
         {
-            if (!context.performed || !IsGround || IsLimitSlope)
+            if (!context.started || !IsGround || IsLimitSlope)
             {
                 return;
             }
-            
-            _moveDirection.y = _jumpSpeed;
 
-            if (!HasInput)
+
+            if (_inputDirection.sqrMagnitude != 0)
             {
-                _controller.Move(_moveDirection * (_moveSpeed * Time.fixedDeltaTime));
+
+                _yVelocity += _jumpPower;
+                return;
             }
+
+            _yVelocity = 0;
+            _yVelocity += _jumpPower * 0.8f;
+            MovePlayer();
         }
 
         private void OnControllerColliderHit(ControllerColliderHit hit)
