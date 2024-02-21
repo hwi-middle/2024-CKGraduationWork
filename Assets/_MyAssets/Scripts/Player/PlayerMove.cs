c3a2a1861b2f78fa6c3061f7681f0f430533c9a5
Jae_U <31125543+thejae-u@users.noreply.github.com>
2024-02-21 22:18:15 +0900

    [#11] 경사로 로직 추가
    경사로에서 미끄러지도록 구현

--- Assets/_MyAssets/Scripts/Player/PlayerMove.cs
+++ Assets/_MyAssets/Scripts/Player/PlayerMove.cs
@@ -8,20 +8,35 @@ namespace _MyAssets.Scripts.Player
     {
         [SerializeField] private float _moveSpeed;
         [SerializeField] private float _jumpSpeed;
+        [SerializeField] private float _slideSpeed;
 
+        private Camera _camera;
+        
         private Vector3 _inputDirection;
 
         private CharacterController _controller;
         private Vector3 _moveDirection;
+        private Vector3 _hitNormal;
 
-        private Camera _camera;
-
+        
         private bool IsGround
         {
             get
             {
                 Debug.Assert(_controller != null, "_controller != null");
+                
                 return _controller.isGrounded;
+            }
+        }
+
+        private bool IsLimitSlope
+        {
+            get
+            {
+                Debug.Assert(_controller != null, "_controller != null");
+
+                float angle = Vector3.Angle(Vector3.up, _hitNormal);
+                return angle <= _controller.slopeLimit && angle != 0;
             }
         }
 
@@ -53,12 +68,21 @@ namespace _MyAssets.Scripts.Player
 
         private void MovePlayer()
         {
+            // 가파른 경사로에서는 미끄러짐
+            if (IsLimitSlope && IsGround)
+            {
+                _moveDirection.x += (1f - _hitNormal.y) * _hitNormal.x;
+                _moveDirection.z += (1f - _hitNormal.y) * _hitNormal.z;
+                _controller.Move(_moveDirection * (_slideSpeed * Time.fixedDeltaTime));
+                return;
+            }
+            
             // 입력이 아무것도 없을 때 공중에 있지 않으면 연산하지 않음
             if (!HasInput && IsGround)
             {
                 return;
             }
-
+            
             _inputDirection.y = _moveDirection.y;
             _moveDirection = transform.TransformDirection(_inputDirection);
             
@@ -90,7 +114,7 @@ namespace _MyAssets.Scripts.Player
 
         public void OnJump(InputAction.CallbackContext context)
         {
-            if (!context.performed || !IsGround)
+            if (!context.performed || !IsGround || IsLimitSlope)
             {
                 return;
             }
@@ -101,6 +125,11 @@ namespace _MyAssets.Scripts.Player
             {
                 _controller.Move(_moveDirection * (_moveSpeed * Time.fixedDeltaTime));
             }
+        }
+
+        private void OnControllerColliderHit(ControllerColliderHit hit)
+        {
+            _hitNormal = hit.normal;
         }
     }
 }
