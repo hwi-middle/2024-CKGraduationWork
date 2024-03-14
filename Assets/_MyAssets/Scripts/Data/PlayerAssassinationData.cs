ef48f102d809c2d3ae091735549e7513c86a40ac
주휘중 <gnlwnd5084@gmail.com>
2024-03-15 01:32:00 +0900

    [#23] Raycast를 통한 에임 시스템 구현

--- Assets/_MyAssets/Scripts/Data/PlayerAssassinationData.cs
+++ Assets/_MyAssets/Scripts/Data/PlayerAssassinationData.cs
@@ -4,5 +4,10 @@ using UnityEngine.Serialization;
 [CreateAssetMenu(fileName = "NewPlayerAssassinationData", menuName = "Scriptable Object Asset/Player Assassination Data")]
 public class PlayerAssassinationData : ScriptableObject
 {
-    [Tooltip("암살 시간")] public float jumpAssassinationDuration;
+    [Header("암살 소요 시간")]
+    [Tooltip("공중 암살 시간")] public float jumpAssassinationDuration;
+    [Tooltip("평지 암살 시간")] public float groundAssassinationDuration;
+    
+    [Header("암살 구분 기준")]
+    [Tooltip("공중 암살 판정 임계값")] public float jumpAssassinationHeightThreshold;
 }
--- Assets/_MyAssets/Scripts/Player/PlayerMoveAssassination.cs
+++ Assets/_MyAssets/Scripts/Player/PlayerMoveAssassination.cs
@@ -6,19 +6,43 @@ using UnityEngine.Serialization;
 
 public class PlayerMoveAssassination : PlayerMove
 {
+    private enum EAssassinationType
+    {
+        Ground,
+        Jump,
+    }
+
     [SerializeField] private PlayerAssassinationData _assassinationData;
-    private float _assassinationDuration;
+    private float _jumpAssassinationDuration;
+    private float _groundAssassinationDuration;
+    private float _jumpAssassinationHeightThreshold;
+
     [SerializeField] private Transform _assassinationTarget;
 
     protected override void Awake()
     {
         base.Awake();
-        _assassinationDuration = _assassinationData.jumpAssassinationDuration;
+
+        _jumpAssassinationDuration = _assassinationData.jumpAssassinationDuration;
+        _groundAssassinationDuration = _assassinationData.groundAssassinationDuration;
+        _jumpAssassinationHeightThreshold = _assassinationData.jumpAssassinationHeightThreshold;
     }
 
     protected override void Update()
     {
         base.Update();
+
+        Camera mainCamera = Camera.main;
+        Debug.Assert(mainCamera != null);
+        Ray ray = mainCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        int layerMask = LayerMask.GetMask("AssassinationTarget");
+        Debug.DrawRay(mainCamera.transform.position, mainCamera.transform.forward * 50f, Color.green, 0f, false);
+        if (Physics.Raycast(ray, out RaycastHit hit, 50f, layerMask))
+        {
+            Debug.Log(hit.transform.name);
+        }
+        
+        // TODO: NIS 적용
         if (Input.GetKeyDown(KeyCode.Q))
         {
             Assassinate();
@@ -27,6 +51,8 @@ public class PlayerMoveAssassination : PlayerMove
 
     private void Assassinate()
     {
+        // if(transform.position.y)
+
         PerformJump();
         StartCoroutine(AssassinateRoutine());
     }
@@ -37,19 +63,19 @@ public class PlayerMoveAssassination : PlayerMove
         {
             yield return null;
         }
-        
+
         float t = 0f;
         Vector3 initialPos = transform.position;
 
-        while (t <= _assassinationDuration)
+        while (t <= _jumpAssassinationDuration)
         {
-            float alpha = t / _assassinationDuration;
+            float alpha = t / _jumpAssassinationDuration;
             transform.position = Vector3.Lerp(initialPos, _assassinationTarget.position, alpha * alpha * alpha);
 
             yield return null;
             t += Time.deltaTime;
         }
-        
+
         // TODO: 실제 적에게 데미지를 입혀야함
     }
 }
