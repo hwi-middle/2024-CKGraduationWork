373c2372785600d44e1aaa0090e8d4d5f7ae18b7
Ju Hwijung <gnlwnd5084@gmail.com>
2024-03-19 13:36:19 +0900

    [#23] 구현 내용 PlayerMove와 병합

--- Assets/_MyAssets/Scripts/Player/PlayerMove.cs
+++ Assets/_MyAssets/Scripts/Player/PlayerMove.cs
@@ -1,8 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
+    private enum EAssassinationType
+    {
+        Ground, // 평지에서 암살
+        Fall, // 위에서 아래로 떨어지며 암살
+        Jump, // 아래에서 위로 점프하며 암살
+    }
+
+    [SerializeField] private PlayerAssassinationData _assassinationData;
+
+    // [SerializeField] private TMP_Text _noteTextForDebug;
+    private bool _isAssassinating = false;
+    private Transform _assassinationTarget;
+
     [SerializeField] private float _jumpHeight;
 
     [SerializeField] private float _moveSpeed;
@@ -47,6 +63,11 @@ public class PlayerMove : MonoBehaviour
         SetSlideVelocity();
         RotatePlayer();
         MovePlayer();
+
+        if (!_isAssassinating)
+        {
+            _assassinationTarget = GetAimingEnemy();
+        }
     }
 
     private void RotatePlayer()
@@ -149,5 +170,106 @@ public class PlayerMove : MonoBehaviour
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
         _hitNormal = hit.normal;
+    }
+
+
+    public void OnAssassinateKeyDown(InputAction.CallbackContext context)
+    {
+        if (!context.started || _assassinationTarget == null)
+        {
+            return;
+        }
+
+        Assassinate();
+    }
+
+    private Transform GetAimingEnemy()
+    {
+        Camera mainCamera = Camera.main;
+        Debug.Assert(mainCamera != null);
+        Ray ray = mainCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        float assassinateDistance = _assassinationData.assassinateDistance;
+        Debug.DrawRay(mainCamera.transform.position, mainCamera.transform.forward * assassinateDistance, Color.green,
+            0f, false);
+        int layerMask = (-1) - (1 << LayerMask.NameToLayer("BypassAiming"));
+
+        if (Physics.Raycast(ray, out RaycastHit hit, assassinateDistance, layerMask) &&
+            hit.transform.CompareTag("AssassinationTarget"))
+        {
+            // _noteTextForDebug.text = $"Current Target: {hit.transform.name}";
+            return hit.transform;
+        }
+
+        // _noteTextForDebug.text = "Current Target: None";
+        return null;
+    }
+
+    private void Assassinate()
+    {
+        Debug.Assert(_assassinationTarget != null);
+
+        EAssassinationType assassinationType;
+        float yPositionDiff = Mathf.Abs(transform.position.y - _assassinationTarget.position.y);
+        if (transform.position.y > _assassinationTarget.position.y &&
+            yPositionDiff >= _assassinationData.fallAssassinationHeightThreshold)
+        {
+            assassinationType = EAssassinationType.Fall;
+        }
+        else if (transform.position.y < _assassinationTarget.position.y &&
+                 yPositionDiff >= _assassinationData.jumpAssassinationHeightThreshold)
+        {
+            assassinationType = EAssassinationType.Jump;
+        }
+        else
+        {
+            assassinationType = EAssassinationType.Ground;
+        }
+
+        StartCoroutine(AssassinateRoutine(assassinationType));
+    }
+
+    private IEnumerator AssassinateRoutine(EAssassinationType assassinationType)
+    {
+        _isAssassinating = true;
+
+        float assassinationDuration = 0f;
+        switch (assassinationType)
+        {
+            case EAssassinationType.Ground:
+                assassinationDuration = _assassinationData.groundAssassinationDuration;
+                break;
+            case EAssassinationType.Jump:
+                assassinationDuration = _assassinationData.jumpAssassinationDuration;
+                break;
+            case EAssassinationType.Fall:
+                assassinationDuration = _assassinationData.fallAssassinationDuration;
+                PerformJump();
+
+                while (YVelocity > 0f)
+                {
+                    yield return null;
+                }
+
+                break;
+            default:
+                Debug.Assert(false);
+                break;
+        }
+
+        float t = 0f;
+        Vector3 initialPos = transform.position;
+
+        while (t <= assassinationDuration)
+        {
+            float alpha = t / assassinationDuration;
+            transform.position = Vector3.Lerp(initialPos, _assassinationTarget.position, alpha * alpha * alpha);
+
+            yield return null;
+            t += Time.deltaTime;
+        }
+
+        // TODO: 실제 적에게 데미지를 입혀야함
+
+        _isAssassinating = false;
     }
 }
