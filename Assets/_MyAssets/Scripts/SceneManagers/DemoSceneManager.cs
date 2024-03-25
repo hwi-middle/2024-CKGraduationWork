ca509ba6c9137fa271fcc09c2b1e3a3b342dcc22
thejae-u <31125543+thejae-u@users.noreply.github.com>
2024-03-25 23:23:37 +0900

    [#43] Setting Canvas 세팅
    Escape Key Bind, Setting Canvas 프리팹화, Setting Canvas Show 로직

--- Assets/_MyAssets/Scripts/SceneManagers/DemoSceneManager.cs
+++ Assets/_MyAssets/Scripts/SceneManagers/DemoSceneManager.cs
@@ -1,11 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class DemoSceneManager : SceneManagerBase
 {
+    private GameObject _settingCanvas;
+    private bool _isPaused;
+
     protected override void Start()
     {
         base.Start();
@@ -15,6 +20,27 @@ public class DemoSceneManager : SceneManagerBase
     protected override void Update()
     {
         base.Update();
+    }
+
+    public void OnPause(InputAction.CallbackContext context)
+    {
+        if (!context.started || IsFading)
+        {
+            return;
+        }
+
+        _isPaused = !_isPaused;
+
+        if (_isPaused)
+        {
+            _settingCanvas = Instantiate(Resources.Load<GameObject>("SettingCanvas"));
+            _settingCanvas.GetComponent<Canvas>().sortingOrder = 10;
+            Time.timeScale = 0.0f;
+            return;
+        }
+
+        Destroy(_settingCanvas);
+        Time.timeScale = 1.0f;
     }
 
     public void OnStartButton()
