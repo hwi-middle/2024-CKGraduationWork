8af6e89d255fa0697507f2c635f9b42ae4a32e46
thejae-u <31125543+thejae-u@users.noreply.github.com>
2024-03-29 12:46:32 +0900

    [#56] 코드 정상화 및 player Prefab 소실 문제 해결

--- Assets/_MyAssets/Scripts/SceneManagers/MainLobbySceneManager.cs
+++ Assets/_MyAssets/Scripts/SceneManagers/MainLobbySceneManager.cs
@@ -9,16 +9,16 @@ public class MainLobbySceneManager : SceneManagerBase
     [SerializeField] private TextMeshProUGUI _versionText;
     [SerializeField] private GameObject _warningText;
 
-    private void Awake()
-    {
-        Instantiate(Resources.Load<GameObject>("FadeCanvas"));
-#if __BUILD_ON_CLOUD
-        _warningText.SetActive(true);
-        _versionText.text = $"Version: {Application.version} (Automated Build)";
-#else
-        _versionText.text = $"Version: {Application.version}";
-#endif
-    }
+//    private void Awake()
+//    {
+//        Instantiate(Resources.Load<GameObject>("FadeCanvas"));
+//#if __BUILD_ON_CLOUD
+//        _warningText.SetActive(true);
+//        _versionText.text = $"Version: {Application.version} (Automated Build)";
+//#else
+//        _versionText.text = $"Version: {Application.version}";
+//#endif
+//    }
 
     protected override void Start()
     {
