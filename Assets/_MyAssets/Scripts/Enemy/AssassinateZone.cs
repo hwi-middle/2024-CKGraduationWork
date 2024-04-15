2cbdded18bb9896eac5731fa310c771a44c84d46
주휘중 <contact@juhwijung.com>
2024-04-16 00:29:17 +0900

    [#92] 암살 날림구현...

---
+++ Assets/_MyAssets/Scripts/Enemy/AssassinateZone.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssassinateZone : MonoBehaviour
+{
+    [SerializeField] private PlayerInputData _playerInputData;
+    [SerializeField] private EnemyBase _targetEnemy;
+    [SerializeField] private GameObject _assassinateUI;
+    private bool _isInZone;
+
+    private void OnEnable()
+    {
+        _playerInputData.assassinateEvent += HandleAssassinateAction;
+    }
+
+    private void OnDisable()
+    {
+        _playerInputData.assassinateEvent -= HandleAssassinateAction;
+    }
+
+    private void HandleAssassinateAction()
+    {
+        if (!_isInZone)
+        {
+            return;
+        }
+        
+        Destroy(_targetEnemy.gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+        _isInZone = true;
+        // _assassinateUI.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+        _isInZone = false;
+        // _assassinateUI.SetActive(false);
+    }
+}
