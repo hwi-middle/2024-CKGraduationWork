d4f45e0152db17063f0c09bc5a66b590afa4006c
thejae-u <31125543+thejae-u@users.noreply.github.com>
2024-05-31 15:20:40 +0900

    [#164] 일시정지 UI 적용 및 2D Sprite 에셋 임포트

--- Assets/_MyAssets/Scripts/Misc/PauseCanvasController.cs
+++ Assets/_MyAssets/Scripts/Misc/PauseCanvasController.cs
@@ -3,29 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class PauseCanvasController : MonoBehaviour
 {
     [SerializeField] private Button _resumeButton;
     [SerializeField] private Button _optionsButton;
+    [SerializeField] private Button _menuButton;
     [SerializeField] private Button _quitButton;
 
     private void OnEnable()
     {
         _resumeButton.onClick.AddListener(HandleResumeButton);
         _optionsButton.onClick.AddListener(HandleSettingsButton);
+        _menuButton.onClick.AddListener(HandleMenuButton);
         _quitButton.onClick.AddListener(HandleQuitButton);
 
         bool isMainMenu = SceneManager.GetActiveScene().name.Equals(SceneNames.MAIN_MENU);
-        _quitButton.gameObject.SetActive(!isMainMenu);
+        _menuButton.gameObject.SetActive(!isMainMenu);
     }
     
     private void OnDisable()
     {
         _resumeButton.onClick.RemoveListener(HandleResumeButton);
         _optionsButton.onClick.RemoveListener(HandleSettingsButton);
-        _quitButton.onClick.RemoveListener(HandleQuitButton);
+        _menuButton.onClick.RemoveListener(HandleMenuButton);
     }
     
     private void HandleResumeButton()
@@ -38,8 +41,17 @@ public class PauseCanvasController : MonoBehaviour
         SceneManagerBase.Instance.OnSettingsButtonClick();
     }
     
-    private void HandleQuitButton()
+    private void HandleMenuButton()
     {
         SceneManagerBase.Instance.OnQuitButtonClick();
+    }
+
+    private void HandleQuitButton()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(0);       
+#endif
     }
 }
