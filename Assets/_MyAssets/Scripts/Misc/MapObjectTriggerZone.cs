e2b549e44417bd3d74d12bd8d20bee7e5d1cbddf
thejae-u <31125543+thejae-u@users.noreply.github.com>
2024-06-04 03:38:28 +0900

    크레딧 연출용 시그널 함수 추가

--- Assets/_MyAssets/Scripts/Misc/MapObjectTriggerZone.cs
+++ Assets/_MyAssets/Scripts/Misc/MapObjectTriggerZone.cs
@@ -21,4 +21,9 @@ public class MapObjectTriggerZone : MonoBehaviour
 
         _animator.enabled = true;
     }
+
+    public void StartSignalAnimation()
+    {
+        _animator.enabled = true;
+    }
 }
