37591b1bd11cf47eb7444548153c0c78cdd38e48
주휘중 <contact@juhwijung.com>
2024-06-03 22:39:22 +0900

    크레딧 씬 제작

---
+++ Assets/_MyAssets/Scripts/SceneManagers/CreditSceneManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditSceneManager : SceneManagerBase
+{
+    private float _originalYPos;
+    [SerializeField] private float _targetYPos;
+    [SerializeField]  private float _scrollDuration;
+    [SerializeField] private RectTransform _creditText;
+    private float _elapsedTime = 0f;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _originalYPos = _creditText.anchoredPosition.y;
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        FadeIn(0.5f);
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        
+        Vector2 textPos = _creditText.anchoredPosition;
+        textPos.y = Mathf.Lerp(_originalYPos, _targetYPos, _elapsedTime / _scrollDuration);
+        _creditText.anchoredPosition = textPos;
+        _elapsedTime += Time.deltaTime;
+    }
+}
