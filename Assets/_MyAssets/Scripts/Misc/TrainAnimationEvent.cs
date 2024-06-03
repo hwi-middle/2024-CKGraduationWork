5d9d3d0565d4df89d76ab409fb95fc1000a2f213
주휘중 <contact@juhwijung.com>
2024-06-04 03:58:46 +0900

    기차 마지막 이벤트 Null 체크

--- Assets/_MyAssets/Scripts/Misc/TrainAnimationEvent.cs
+++ Assets/_MyAssets/Scripts/Misc/TrainAnimationEvent.cs
@@ -11,6 +11,10 @@ public class TrainAnimationEvent : MonoBehaviour
     public void OnTrainAnimationEnd()
     {
         _trainRailSparkEffect.SetActive(false);
-        _enemy.SetActive(true);
+
+        if (_enemy != null)
+        {
+            _enemy.SetActive(true);
+        }
     }
 }
